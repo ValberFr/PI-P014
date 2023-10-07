060ea356d2ca14bedf2c5089dead4b3c90eeb672
joaopedropinto <jpcic07@gmail.com>
2023-10-06 21:50:21 -0300

    ajustes

--- Exercicio5.cpp
+++ Exercicio5.cpp
@@ -3,38 +3,36 @@
 
 using namespace std;
 
-// Struct para representar uma CPU (Processador)
 struct CPU {
     string marca;
     int nucleos;
-    double frequencia; // Frequencia em GHz
+    double frequencia; 
 };
 
-// Struct para representar uma Memoria RAM
 struct RAM {
     string tipo;
-    int capacidade; // Capacidade em GB
+    int capacidade; 
 };
 
-// Struct para representar um dispositivo de Armazenamento (HD/SSD)
+
 struct Armazenamento {
     string tipo;
-    int capacidade; // Capacidade em GB
+    int capacidade; 
 };
 
-// Struct para representar uma GPU (Placa de Video)
+
 struct GPU {
     string marca;
-    int memoria; // Memoria em GB
+    int memoria; 
 };
 
-// Struct para representar um Sistema Operacional
+
 struct SistemaOperacional {
     string nome;
     string versao;
 };
 
-// Struct principal que combina todas as anteriores para formar um Computador
+
 struct Computador {
     CPU processador;
     RAM memoria;
@@ -44,16 +42,14 @@ struct Computador {
 };
 
 int main() {
-    // Definindo e inicializando um computador
     Computador meuPc = {
-        { "Intel", 8, 3.5 },            // Informacoes da CPU
-        { "DDR4", 16 },                 // Informacoes da RAM
-        { "SSD", 500 },                 // Informacoes de Armazenamento
-        { "NVIDIA", 6 },                // Informacoes da GPU
-        { "Windows", "11 Pro" }         // Informacoes do Sistema Operacional
+        { "Intel", 8, 3.5 },            
+        { "DDR4", 16 },                 
+        { "SSD", 500 },                 
+        { "NVIDIA", 6 },                
+        { "Windows", "11 Pro" }         
     };
 
-    // Exibindo informacoes sobre o computador
     cout << "Informacoes do Computador:\n";
     cout << "Processador: " << meuPc.processador.marca << ", Nucleos: " << meuPc.processador.nucleos << ", Frequencia: " << meuPc.processador.frequencia << " GHz" << endl;
     cout << "Memoria RAM: " << meuPc.memoria.tipo << ", Capacidade: " << meuPc.memoria.capacidade << " GB" << endl;
